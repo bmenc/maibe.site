1ecb1c17de0aea40328efd27d368a704d44601b4
bmenc <bmenchaca.cca@gmail.com>
2025-04-14 21:52:51 -0600

    refactor: replace MenuExample with Menu component and restructure Home component

---
+++ app/components/FieldGrid.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useState } from "react";
+import { Card, FormGroup, InputGroup } from "@blueprintjs/core";
+
+interface FieldGridProps {
+  columns: number;
+  fields: string[];
+  addField: () => void;
+  updateColumns: (newColumns: number) => void;
+}
+
+export default function FieldGrid({ columns, fields }: FieldGridProps) {
+  const getColumnClass = (columns: number) => {
+    return `grid-cols-${columns || 1}`;
+  };
+
+  return (
+    <Card className="w-full">
+      <div className={`grid ${getColumnClass(columns)} gap-2`}>
+        {fields.map((field, index) => (
+          <FormGroup
+            key={index}
+            label={`Field ${index + 1}`}
+            labelFor={String(index)}
+            helperText={`Helper text for field ${index + 1}`}
+            fill={false}
+            inline={false}
+            className="flex-1"
+          >
+            <InputGroup id={String(index)} placeholder="Placeholder text" />
+          </FormGroup>
+        ))}
+      </div>
+    </Card>
+  );
+}
--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -4,11 +4,11 @@ import * as React from "react";
 
 import { Button, NumericInput } from "@blueprintjs/core";
 
-export function MenuExample({ addField, updateColumns }: { addField: (field: string) => void; updateColumns: (columns: number) => void }) {
+export function Menu({ addField, updateColumns }: { addField: (field: string) => void; updateColumns: (columns: number) => void }) {
     return (
-        <div className="flex justify-between gap-1 w-full p-1">
+        <div className="flex justify-between gap-1 w-full px-4">
             <div className="flex gap-1">
-                <Button icon="new-text-box" text="New text box" onClick={() => addField("textBox")} />
+                <Button icon="new-text-box" text="New field" onClick={() => addField("textBox")} />
                 <NumericInput
                     min={1}
                     max={6}
@@ -17,8 +17,7 @@ export function MenuExample({ addField, updateColumns }: { addField: (field: str
                 />
             </div>
             <div className="flex items-center gap-2">
-
-                <Button icon="download" text="Download tsx" />
+                <Button icon="download" text="Download" />
             </div>
         </div>
     );
