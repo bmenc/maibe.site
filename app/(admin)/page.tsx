0785fda05e9d210e6f0bd741827c49919ae31dc0
bmenc <benjamin.menchaca@imedmex.com.mx>
2025-04-18 17:18:15 -0600

    refactor: remove FieldGrid and Menu components; replace with FormGroupExample for simplified layout

--- app/(admin)/page.tsx
+++ app/(admin)/page.tsx
@@ -1,45 +1,12 @@
 "use client";
-import { useState } from "react";
-import { Menu } from "@/app/components/menu";
-import FieldGrid from "@/app/components/FieldGrid";
+import { FormGroupExample } from "@/app/components/examples/formGroupExample";
+import { FieldType } from "@/app/components/examples/formGroupExample";
 
 export default function Home() {
-  const [config, setConfig] = useState({
-    fields: [] as string[],
-    columns: 5,
-  });
-  const [editingAll, setEditingAll] = useState(false);
-
-  const addField = () => {
-    setConfig((prevConfig) => ({
-      ...prevConfig,
-      fields: [...prevConfig.fields, ""],
-    }));
-  };
-
-  const updateColumns = (newColumns: number) => {
-    setConfig((prevConfig) => ({
-      ...prevConfig,
-      columns: newColumns,
-    }));
-  };
-
-  const toggleAllEditButtons = () => {
-    setEditingAll((prev) => !prev);
-  };
 
   return (
-    <main className="flex flex-col mt-4">
-      <Menu addField={addField} updateColumns={updateColumns} toggleAllEditButtons={toggleAllEditButtons} />
-      <div className="flex flex-col justify-between items-center p-4 mt-2">
-        <FieldGrid 
-          columns={config.columns} 
-          fields={config.fields} 
-          addField={addField} 
-          updateColumns={updateColumns} 
-          editingAll={editingAll} 
-        />
-      </div>
+    <main className="w-full p-2">
+      <FormGroupExample id="" p_type={FieldType.TEXT} p_label="Nombre" p_disabled={false} />
     </main>
   );
 }
