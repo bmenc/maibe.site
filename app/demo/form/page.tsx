5e6bb5d73388959361dfa527e63abc3297cf3fc0
bmenc <benjamin.menchaca@imedmex.com.mx>
2025-04-21 10:25:24 -0600

    feat: add donation management table with export functionality to Excel and PDF

---
+++ app/demo/form/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import * as React from "react";
+import { FormGroup } from "./formGroup";
+import { formFields } from "./data";
+import PrevisualizerJSON from "@/app/components/PrevisualizerJSON";
+import {FormGroupExample} from "@/app/components/blueprint-core/formGroupExample";
+import SimplePhoneInput from "./InputMask"
+
+export default function FormPage() {
+  const title = "Formulario de prueba";
+  const [data] = React.useState(formFields);
+
+  return (
+    <main className="p-4 flex flex-col gap-2">
+      <FormGroupExample id="" />
+      <FormGroup title={title} />
+      <PrevisualizerJSON data={data} />
+      <SimplePhoneInput />
+    </main>
+  );
+}
