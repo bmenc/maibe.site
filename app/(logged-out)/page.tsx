f9e31f572a1956370ee713d4cdbfdeb8cf7962ea
bmenc <bmenchaca.cca@gmail.com>
2025-05-02 21:41:15 -0600

    fix: update Link className for consistent styling on LandingPage

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function LandingPage() {
   return (
     <section className="flex-group">
-      <Link href="/login" className="flex-group">
-        <Button text={"Log In"}/>
+      <Link href="/login" className="no-underline">
+        <Button text="Log In" />
       </Link>
     </section>
   );
